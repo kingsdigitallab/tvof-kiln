b69b017e4f17467487c6f21bfb924bf9e8432699
geoffroy-noel-ddh <geoffroy.noel@kcl.ac.uk>
2018-05-13 18:39:38 +0100

    dropbox2tomcat now merges the alignment files into Tomcat

--- KWIC/tokenise/dropbox2tomcat.sh
+++ KWIC/tokenise/dropbox2tomcat.sh
@@ -4,6 +4,6 @@ python2 doall.py edition/fr20125/source/{1..100}[_,-]*.xml -c -o fr/ && cp fr/co
 python2 doall.py edition/royal/source/{1..100}[_,-]*.xml -c -o royal/ && cp royal/converted.xml tomcat/Royal.xml
 #cp edition/Modeling_Structure_Segmentation/TVOF_para_alignment.xml ../../webapps/ROOT/content/xml/tei/alists/TVOF_para_alignment.xml
 # python2 align_merge.py "alignment/source/TVOF_para_alignment*.xml" -o ../../webapps/ROOT/content/xml/tei/alists/TVOF_para_alignment.xml
-python2 align_merge.py "alignment/source/TVOF_para_alignment_Fr20125.xml" "alignment/source/TVOF_para_alignment_Royal_20_D_1.xml" "alignment/source/TVOF_para_alignment_Add_15268.xml" "alignment/source/TVOF_para_alignment_Add_19669.xml" alignment/source/TVOF_para_alignment_Fr17177.xml -o ../../webapps/ROOT/content/xml/tei/alists/TVOF_para_alignment.xml
+python2 align_merge.py "edition/Modeling_Structure_Segmentation/source/TVOF_para_alignment_Fr20125.xml" "edition/Modeling_Structure_Segmentation/source/TVOF_para_alignment_Royal_20_D_1.xml" "edition/Modeling_Structure_Segmentation/source/TVOF_para_alignment_Add_15268.xml" "edition/Modeling_Structure_Segmentation/source/TVOF_para_alignment_Add_19669.xml" edition/Modeling_Structure_Segmentation/source/TVOF_para_alignment_Fr17177.xml -o ../../webapps/ROOT/content/xml/tei/alists/TVOF_para_alignment.xml
 cd ../..
 ./force_reload.sh
