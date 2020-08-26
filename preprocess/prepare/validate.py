482dd1f7c7c930e2d10bf8fb6a34efaa246afc23
geoffroy-noel-ddh <geoffroy.noel@kcl.ac.uk>
2020-08-26 20:23:26 +0100

    ac-393, dev, a) Xpath to validate format of <seg>/<div> xml:ids

--- preprocess/prepare/validate.py
+++ preprocess/prepare/validate.py
@@ -26,17 +26,19 @@ class Validator(XMLParser):
                 with io.open(input_path, mode="r", encoding="utf-8") as f:
                     content = f.read()
 
-                try:
-                    self.set_xml_from_unicode(content)
-                except ET.ParseError as e:
-                    # xml.etree.ElementTree.ParseError:
-                    # mismatched tag: line 40825, column 18
-                    self.add_error(
-                        'XML validation error: "%s" in %s'
-                        % (e.message, input_path)
-                    )
+                # 1. XML validity
+                if 1:
+                    try:
+                        self.set_xml_from_unicode(content)
+                    except ET.ParseError as e:
+                        # xml.etree.ElementTree.ParseError:
+                        # mismatched tag: line 40825, column 18
+                        self.add_error(
+                            'XML validation error: "%s" in %s'
+                            % (e.message, input_path)
+                        )
 
-                # 1. erase the comments
+                # 2. erase the comments
                 # (so we know there won't be special chars there)
 
                 def sub_comment(match):
@@ -47,36 +49,86 @@ class Validator(XMLParser):
                 content = re.sub(r'(?musi)<!--(.*?)-->',
                                  sub_comment, content)
 
-                # 2. find all special chars
+                # 2. ids
+                self.validate_ids(content, input_path)
 
-                special_chars = u'@%±≠€~`≤Ω{}$*£'
-
-                pattern_char = u'[' + re.escape(special_chars) + u']'
-                pattern_cr = re.compile(r'[\n\r]')
-                pattern_element = re.compile(
-                    r'(?musi)<\s*(\w+)\s+[^>]+id="([^"]+)"')
-
-                for match_char in re.finditer(pattern_char, content):
-                    # print(match_char.start(), match_char.group(0))
-                    line_number = len(pattern_cr.findall(
-                        content, 0, match_char.start()
-                    )) + 1
-                    match_elements = pattern_element.findall(
-                        content, 0, match_char.start()
-                    )
-                    if match_elements:
-                        self.add_error(
-                            u'%s found in %s id="%s", %s line %s' %
-                            (
-                                match_char.group(),
-                                match_elements[-1][0],
-                                match_elements[-1][1],
-                                input_path,
-                                line_number,
-                            )
-                        )
+                # 3. find all special chars
+                self.validate_special_chars(content, input_path)
 
             self.write_errors(output_path)
+
+    def validate_ids(self, content, input_path):
+        '''Validate the format of the ids in div and seg elements
+          <div n="3ra" type="1" xml:id="edfr20125_00002">
+          <seg type="5" xml:id="edfr20125_00002_01">
+        '''
+
+        is_royal = re.search('(?i)<title\s+type="main">[^<]*royal', content)
+        prefix = 'edfr20125_'
+        if is_royal:
+            prefix = 'edRoyal20D1_'
+
+        idx = 1
+        last_div_id = '_0'
+        last_seg_idx = 0
+        for line in content.split('\n'):
+            if 'type="notes"' in line:
+                break
+            ids = re.findall(r'<(\w+)[^>]+id="([^"]+)', line)
+            for aid in ids:
+                if aid[0] == 'div':
+                    if not re.match(prefix+r'\d{5}$', aid[1]):
+                        print('WARNING: div id format is invalid, %s (line %s, %s)' % (aid[1], idx, input_path))
+                    else:
+                        if int(aid[1].split('_')[-1]) != 1 + int(last_div_id.split('_')[-1]):
+                            print(
+                                'WARNING: div id is out of sequence, %s (line %s, %s)' % (
+                                aid[1], idx, input_path)
+                            )
+                        last_div_id = aid[1]
+                        last_seg_idx = 0
+                if aid[0] == 'seg':
+                    expected_seg_id = '%s_%02d' % (last_div_id, last_seg_idx + 1)
+                    if not re.match(prefix+r'\d{5}_\d{2}$', aid[1]):
+                        print(
+                            'WARNING: seg id format is invalid, %s (line %s, %s)' % (aid[1], idx, input_path))
+                    elif not aid[1].startswith(last_div_id):
+                        print(
+                            'WARNING: seg id doesn\'t start with div id, %s (line %s, %s)' % (aid[1], idx, input_path))
+                    elif not aid[1] == expected_seg_id:
+                        print(
+                            'WARNING: seg id out of sequence, %s, expected %s (line %s, %s)' % (aid[1], expected_seg_id, idx, input_path))
+                    last_seg_idx += 1
+
+            idx += 1
+
+    def validate_special_chars(self, content, input_path):
+        special_chars = u'@%±≠€~`≤Ω{}$*£'
+
+        pattern_char = u'[' + re.escape(special_chars) + u']'
+        pattern_cr = re.compile(r'[\n\r]')
+        pattern_element = re.compile(
+            r'(?musi)<\s*(\w+)\s+[^>]+id="([^"]+)"')
+
+        for match_char in re.finditer(pattern_char, content):
+            # print(match_char.start(), match_char.group(0))
+            line_number = len(pattern_cr.findall(
+                content, 0, match_char.start()
+            )) + 1
+            match_elements = pattern_element.findall(
+                content, 0, match_char.start()
+            )
+            if match_elements:
+                self.add_error(
+                    u'%s found in %s id="%s", %s line %s' %
+                    (
+                        match_char.group(),
+                        match_elements[-1][0],
+                        match_elements[-1][1],
+                        input_path,
+                        line_number,
+                    )
+                )
 
     def write_errors(self, output_path):
         if self.validation_errors:
